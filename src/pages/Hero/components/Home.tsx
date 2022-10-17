226aec9b68c12844dd277d616dfa9ba92f82901f
Nikhil <nikhil@tartanhq.com>
2022-10-18 00:27:02 +0530

    audio added

--- src/pages/Hero/components/Home.tsx
+++ src/pages/Hero/components/Home.tsx
@@ -1,9 +1,14 @@
-import { Flex, Text } from "@chakra-ui/react";
+import { Button, Flex, Image, Text } from "@chakra-ui/react";
 import { motion } from "framer-motion";
 import React from "react";
 import { BiDownArrowAlt } from "react-icons/bi";
 
 export default function Home() {
+  const text = [
+    ["H", "i", ";"],
+    ["I", "'", "m", " ", "Logo", "i", "k", "h", "i", "l", ","],
+    ["w", "e", "b", " ", "d", "e", "v", "e", "l", "o", "p", "e", "r"],
+  ];
   return (
     <Flex
       w="100%"
@@ -11,7 +16,161 @@ export default function Home() {
       direction={"column"}
       justifyContent={"space-between"}
     >
-      <Flex></Flex>
+      <Flex
+        w="100%"
+        h="100%"
+        p={{ base: "10px 0px", xl: "20px 40px" }}
+        justifyContent={"space-between"}
+        direction="column"
+        color={"#515A5A"}
+        fontFamily={`'Caveat', cursive;`}
+        fontSize={{
+          base: "20px",
+          xs: "22px",
+          sm: "23px",
+          md: "24px",
+          lg: "25px",
+          xl: "25px",
+        }}
+      >
+        <Flex direction={"column"}>
+          <Text>{"<html>"}</Text>
+          <Text ml={{ base: "10px", md: "20px" }}>{"<body>"}</Text>
+        </Flex>
+        <Flex direction={"column"}>
+          <Text ml={{ base: "10px", md: "20px" }}>{"<h1>"}</Text>
+          <Flex
+            ml={{ base: "30px", md: "60px" }}
+            color="white"
+            fontWeight={"700"}
+            fontSize={{
+              base: "30px",
+              xs: "40px",
+              sm: "50px",
+              md: "70px",
+              lg: "80px",
+              xl: "90px",
+            }}
+            lineHeight={{
+              base: "30px",
+              xs: "40px",
+              sm: "50px",
+              md: "70px",
+              lg: "80px",
+              xl: "90px",
+            }}
+            fontFamily={`'Rubik', sans-serif;`}
+            direction="column"
+          >
+            {text.map((row, ids) => {
+              return (
+                <Flex key={ids}>
+                  {row.map((col, id) => {
+                    if (col == "Logo")
+                      return (
+                        <Flex
+                          alignItems="center"
+                          key={id}
+                          transform={{
+                            base: "translateY(-17px)",
+                            sm: "translateY(-20px)",
+                            md: "translateY(-15px)",
+                          }}
+                          mb="0"
+                          p="0"
+                          m={{
+                            base: "3px",
+                            xs: "3px",
+                            sm: "4px",
+                            md: "5px",
+                            lg: "5px",
+                            xl: "5px",
+                          }}
+                        >
+                          <Image
+                            p={"0"}
+                            m="0"
+                            src="letterN.png"
+                            width={{
+                              base: "30px",
+                              xs: "33px",
+                              sm: "40px",
+                              md: "45px",
+                              lg: "53px",
+                              xl: "63px",
+                            }}
+                            height={{
+                              base: "40px",
+                              xs: "50px",
+                              sm: "60px",
+                              md: "70px",
+                              lg: "80px",
+                              xl: "90px",
+                            }}
+                          />
+                        </Flex>
+                      );
+                    if (col == " ")
+                      return (
+                        <Text
+                          key={id}
+                          mx={{
+                            base: "5px",
+                            xs: "6px",
+                            sm: "7px",
+                            md: "10px",
+                            lg: "14px",
+                            xl: "15px",
+                          }}
+                        />
+                      );
+                    return (
+                      <Text
+                        m="0"
+                        p="0"
+                        key={id}
+                        transform={
+                          ids == 2 ? { base: "translateY(-10px)" } : {}
+                        }
+                      >
+                        {col}
+                      </Text>
+                    );
+                  })}
+                </Flex>
+              );
+            })}
+          </Flex>
+          <Text ml={{ base: "20px", md: "40px" }}>{"<p>"}</Text>
+          <Text
+            ml={{ base: "30px", md: "60px" }}
+            color="#7B7D7D"
+            fontSize={{
+              base: "12px",
+              xs: "14px",
+              sm: "16px",
+              md: "18px",
+            }}
+            letterSpacing={"0.1em"}
+            fontFamily={`'Rubik', sans-serif;`}
+          >
+            {"Front End Developer / Web Developer"}
+          </Text>
+          <Text ml={{ base: "20px", md: "40px" }}>{"</p>"}</Text>
+          <Button
+            ml={{ base: "30px", md: "60px" }}
+            color="#7B7D7D"
+            fontSize={"18px"}
+            letterSpacing={"0.1em"}
+            fontFamily={`'Rubik', sans-serif;`}
+            w="200px"
+            mt={"30px"}
+          >
+            Contact Me
+          </Button>
+        </Flex>
+        <Flex />
+      </Flex>
       <Flex p={"10px"} w="100%" justifyContent={"space-between"}>
         <Flex direction={"column"}>
           <Flex
