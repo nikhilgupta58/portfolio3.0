15777342bd06f429098e15d5ca8a027593f45ac7
Nikhil <nikhil@tartanhq.com>
2022-10-20 19:50:36 +0530

    project json

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,14 +1,40 @@
-import { Flex } from "@chakra-ui/react";
+import { Flex, Image } from "@chakra-ui/react";
 import React from "react";
-import {
-  client
-} from "../utils/contenful";
+import { client } from "../utils/contenful";
+import { getFileLink } from "../utils/lib";
+import { IProjectField, IProjectProp, IProjects } from "../utils/type";
 
 export default function Project() {
+  const [projects, setProjects] = React.useState<IProjectProp[]>([]);
+
   React.useEffect(() => {
-    const posts = client.getEntries({
-      content_type: "project",
-    });
+    (async () => {
+      const posts: IProjects | any = await client.getEntries({
+        content_type: "projects",
+      });
+      let projects: any[] = [];
+      posts?.items?.map((row: IProjectField) => {
+        const item: any = {};
+        const images: string[] = [];
+        item["id"] = row.fields.id;
+        item["name"] = row.fields.name;
+        item["link"] = row.fields.link;
+        item["preview"] = getFileLink(
+          row.fields.preview.sys.id,
+          posts.includes.Asset
+        );
+        row.fields.images.map((row) => {
+          images.push(getFileLink(row.sys.id, posts.includes.Asset));
+        });
+        item["images"] = images;
+        projects.push(item);
+      });
+      projects = projects.sort(function (a, b) {
+        return a.id - b.id;
+      });
+      console.log(projects);
+      setProjects(projects);
+    })();
   }, []);
 
   return (
--- src/utils/contenful.ts
+++ src/utils/contenful.ts
@@ -1,4 +1,4 @@
-var contentful = require("contentful");
+import { createClient } from "contentful";
 
 export const CONTENTFUL_ACCESS_TOKEN = import.meta.env
   .VITE_CONTENTFUL_ACCESS_TOKEN;
@@ -7,7 +7,7 @@ export const CONTENTFUL_SPACE_ID = import.meta.env
 
 export const endpoint = `https://graphql.contentful.com/content/v1/spaces/${CONTENTFUL_SPACE_ID}/explore?access_token=${CONTENTFUL_ACCESS_TOKEN}&enviornmentid=master`;
 
-export const client = contentful.createClient({
+export const client = createClient({
   //@ts-ignore
   space: CONTENTFUL_SPACE_ID,
   environment: "master", // defaults to 'master' if not set
