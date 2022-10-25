c9b14dcc18864e2e441df7d35f7665177187bb02
Nikhil <nikhil@tartanhq.com>
2022-10-25 13:49:32 +0530

    contact

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,4 +1,5 @@
 import { Flex, Text } from "@chakra-ui/react";
+import BgTag from "./BgTag";
 import FooterHTML from "./FooterHTML";
 import HeadTag from "./HeadTag";
 import HtmlTag from "./HtmlTag";
@@ -12,11 +13,19 @@ export default function Contact() {
       mt={"100px"}
       direction={{ base: "column", md: "row" }}
       alignItems={{ base: "self-start", md: "center" }}
+      position="relative"
     >
+      <BgTag
+        text="@"
+        top={{ base: "0", md: "50%" }}
+        left={"50%"}
+        transform="translate(-50%,-50%)"
+      />
       <Flex
         w={{ base: "100%", md: "50%" }}
         direction="column"
         p={{ base: "10px 0px", xl: "20px 40px" }}
+        zIndex={10}
       >
         <HeadTag text="Contact" />
         <HtmlTag tag="<p>" ml={{ base: "20px", md: "40px" }} />
@@ -27,9 +36,8 @@ export default function Contact() {
           fontWeight={600}
           mr="10px"
         >
-          I’m interested in freelance opportunities – especially ambitious or
-          large projects. However, if you have other request or question, don’t
-          hesitate to use the form.
+          I’m interested in any opportunities. However, if you have other
+          request or question, don’t hesitate to use the form.
         </Text>
         <HtmlTag tag="</p>" ml={{ base: "20px", md: "40px" }} />
         <Flex display={{ base: "none", md: "inherit" }}>
@@ -41,6 +49,7 @@ export default function Contact() {
         height={{ base: "300px", md: "100%" }}
         opacity={0.5}
         p={{ base: "10px 30px", md: "0px 0px" }}
+        zIndex={10}
       >
         <Map />
       </Flex>
