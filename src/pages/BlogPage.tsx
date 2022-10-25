ab97a9b33591e17479f59eee676075a4e2cb69c4
Nikhil <nikhil@tartanhq.com>
2022-10-25 16:55:09 +0530

    blog

--- src/pages/BlogPage.tsx
+++ src/pages/BlogPage.tsx
@@ -1,7 +1,7 @@
-import { Flex } from "@chakra-ui/react";
+import { Flex, Grid, GridItem } from "@chakra-ui/react";
 import { Helmet } from "react-helmet";
-import FooterHTML from "../components/FooterHTML";
-import HeaderHTML from "../components/HeaderHTML";
+import BlogBox from "../components/BlogBox";
+import { IBlogProp } from "../utils/type";
 import { useHeroContext } from "./Hero/utils/context";
 
 export default function BlogPage() {
@@ -13,11 +13,38 @@ export default function BlogPage() {
         <meta charSet="utf-8" />
         <title>Blog | Frontend Developer</title>
       </Helmet>
-      <Flex direction={"column"}>
-        <Flex direction="column">
-          <HeaderHTML />
-          {/* <Blog /> */}
-          <FooterHTML />
+      <Flex
+        direction={"column"}
+        w="100%"
+        justifyContent={"center"}
+        alignItems="center"
+      >
+        <Flex direction="column" w="90%" justifyContent={"center"}>
+          <Grid
+            templateColumns={{
+              base: "repeat(1, 1fr)",
+              sm: "repeat(2, 1fr)",
+              md: "repeat(3, 1fr)",
+            }}
+            my={"100px"}
+            zIndex={10}
+            gridGap={{
+              base: "20px",
+              sm: "20px",
+              md: "30px",
+              lg: "40px",
+              xl: "50px",
+            }}
+            w={{ base: "initial", md: "100%" }}
+          >
+            {blogs.map((row: IBlogProp, id: any) => {
+              return (
+                <GridItem key={id} width="100%">
+                  <BlogBox data={row} />
+                </GridItem>
+              );
+            })}
+          </Grid>
         </Flex>
       </Flex>
     </>
